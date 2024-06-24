a634f696dcc3dedd639e40d5cdb9cc7fcd3b7946
Jin Kwon <onacit@gmail.com>
2024-06-24 13:32:23 +0900

    wip

--- src/test/java/com/github/jinahya/mysql/employees/persistence/Department_Basic_Test.java
+++ src/test/java/com/github/jinahya/mysql/employees/persistence/Department_Basic_Test.java
@@ -1,5 +1,25 @@
 package com.github.jinahya.mysql.employees.persistence;
 
+/*-
+ * #%L
+ * mysql-employees-persistece
+ * %%
+ * Copyright (C) 2024 Jinahya, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import nl.jqno.equalsverifier.Warning;
 import nl.jqno.equalsverifier.api.SingleTypeEqualsVerifierApi;
 
@@ -14,8 +34,8 @@ class Department_Basic_Test
     @Override
     SingleTypeEqualsVerifierApi<Department> equals__(final SingleTypeEqualsVerifierApi<Department> verifierApi) {
         return super.equals__(verifierApi)
-                .suppress(Warning.SURROGATE_KEY)
-                .withPrefabValues(Employee.class, Employee.of(0), Employee.of(1))
+                    .suppress(Warning.SURROGATE_KEY)
+                    .withPrefabValues(Employee.class, Employee.of(0), Employee.of(1))
                 ;
     }
 }
--- src/test/java/com/github/jinahya/mysql/employees/persistence/DeptEmpId_Test.java
+++ src/test/java/com/github/jinahya/mysql/employees/persistence/DeptEmpId_Test.java
@@ -1,5 +1,25 @@
 package com.github.jinahya.mysql.employees.persistence;
 
+/*-
+ * #%L
+ * mysql-employees-persistece
+ * %%
+ * Copyright (C) 2024 Jinahya, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import nl.jqno.equalsverifier.Warning;
 import nl.jqno.equalsverifier.api.SingleTypeEqualsVerifierApi;
 import org.junit.jupiter.api.DisplayName;
@@ -27,7 +47,7 @@ class DeptEmpId_Test
     @Override
     SingleTypeEqualsVerifierApi<DeptEmpId> equals__(final SingleTypeEqualsVerifierApi<DeptEmpId> verifierApi) {
         return super.equals__(verifierApi)
-                .suppress(Warning.NONFINAL_FIELDS);
+                    .suppress(Warning.NONFINAL_FIELDS);
     }
 
     // -----------------------------------------------------------------------------------------------------------------
